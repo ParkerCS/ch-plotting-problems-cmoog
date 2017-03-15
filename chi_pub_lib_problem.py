cf5c669df25d5cf1ebca2510b08295e127a242c6
Charlie <cmoog@fwparker.org>
2017-03-15 09:01:01 -0500

    //

--- chi_pub_lib_problem.py
+++ chi_pub_lib_problem.py
@@ -12,7 +12,7 @@ import csv
 from operator import itemgetter
 import matplotlib.pyplot as plt
 import numpy as np
-import matplotlib.patches
+import matplotlib.patches as mpatches
 
 file = open("chilib_visitors_2016")
 reader = csv.reader(file, delimiter='\t')
@@ -41,43 +41,43 @@ month_totals.remove(month_totals[12])
 
 '''Which are the three most visited??'''
 top_three = []  #will first be two dimensional [location index, location_total]
+top_three_index = []
 for l in range(len(lib_data)):  #locaiton
     total = 0
     for g in range(len(lib_data[l]) - 1):  #month
         total += int(lib_data[l][g])
-    if len(top_three) != 3:
-        top_three.append([l,total])
-    elif total > top_three[0][1]:
-        top_three.remove(top_three[0])
-        top_three.append([l, total])
-    elif total > top_three[1][1]:
-        top_three.remove(top_three[1])
-        top_three.append([l, total])
-    elif total > top_three[2][1]:
-        top_three.pop(top_three[2])
-        top_three.append([l, total])
+    if len(top_three) < 3:
+        top_three.append(total)
+        top_three_index.append(l)
+    elif total > min(top_three):
+        top_three_index.remove(top_three_index[top_three.index(min(top_three))])
+        top_three.remove(min(top_three))
+        top_three.append(total)
+        top_three_index.append(l)
 
-top_three = [top_three[0][0], top_three[1][0], top_three[2][0]]
-top_three_index = top_three
-top_three = [lib_data[top_three[0]], lib_data[top_three[1]], lib_data[top_three[2]]]
+top_three = [lib_data[top_three_index[0]], lib_data[top_three_index[1]], lib_data[top_three_index[2]]]
 
-ax = plt.gca()
 plt.xticks(np.arange(len(month_totals)), headers, rotation=20)
 
-total_line = plt.plot(month_totals)
+total_line = plt.plot(month_totals, color="blue")
 
 top_three_index = [locations[top_three_index[0]], locations[top_three_index[1]], locations[top_three_index[2]]]
-print(top_three_index)
 
-top_three_line1 = plt.plot(top_three[0][:12])
-top_three_line2 = plt.plot(top_three[1][:12])
-top_three_line3 = plt.plot(top_three[2][:12])
+top_three_line1 = plt.plot(top_three[0][:12], color="green")
+top_three_line2 = plt.plot(top_three[1][:12], color="red")
+top_three_line3 = plt.plot(top_three[2][:12], color="orange")
 
 plt.xlabel("Months of 2016")
 plt.ylabel("Monthly Visitors")
 plt.title("Chicago Public Library Visitors by Month")
 
-plt.legend([total_line, top_three_line1])#'''FIX THIS'''
+lib_tot_pat = mpatches.Patch(label="Total",color="blue")
+top_three_line1_patch = mpatches.Patch(label=str(top_three_index[0]), color="green")
+top_three_line2_patch = mpatches.Patch(label=str(top_three_index[1]), color="red")
+top_three_line3_patch = mpatches.Patch(label=str(top_three_index[2]), color="orange")
+
+plt.legend(handles=[lib_tot_pat, top_three_line1_patch, top_three_line2_patch, top_three_line3_patch])
+
 
 plt.ylim([0, 870000])
 plt.show()
